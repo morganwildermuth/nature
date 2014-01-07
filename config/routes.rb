69a8a490e9a036b3dcfc461a35fc3fae7a926fc3
morganwildermuth <morgan.wildermuth@gmail.com>
2014-01-07 00:18:56 -0500

    added route for controller games + route to game#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Nature::Application.routes.draw do
+  resources :game
+  root :to => "game#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
